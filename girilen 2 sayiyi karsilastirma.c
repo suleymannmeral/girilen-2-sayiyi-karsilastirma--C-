342dec240269d53e9306eb9ad1827e903443ca86
suleymannmeral <115658595+suleymannmeral@users.noreply.github.com>
2022-10-28 00:10:53 +0300

    girilen 2 sayiyi karsilastirma

---
+++ girilen 2 sayiyi karsilastirma.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{ 
+ int sayi1,sayi2;
+printf("sayi1 giriniz: ") ;  scanf("%d" , &sayi1) ;
+printf("sayi2 giriniz: ") ;  scanf("%d" , &sayi2) ;
+if(sayi1>sayi2)  printf("sayi1 buyuktur\n ") ;
+if(sayi1<sayi2)  printf("sayi2 buyuktur\n ") ;
+if(sayi1==sayi2) printf("sayilar esittir\n ");
+
+return main() ;
+
+} 
+
